3247046e4fb0d395b7f1828a692c979909a5c41a
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-11-12 10:06:10 +0100

    add InternshipData beans

---
+++ src/main/java/model/InternshipData.java
@@ -0,0 +1,85 @@
+package model;
+
+public class InternshipData {
+    private Comments comments;
+    private Company company;
+    private FinalReport finalReport;
+    private Internship internship;
+    private Marks marks;
+    private Skills skills;
+    private Student student;
+    private Tutor tutor;
+    private Visit visit;
+
+    public Comments getComments() {
+        return comments;
+    }
+
+    public void setComments(Comments comments) {
+        this.comments = comments;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public FinalReport getFinalReport() {
+        return finalReport;
+    }
+
+    public void setFinalReport(FinalReport finalReport) {
+        this.finalReport = finalReport;
+    }
+
+    public Internship getInternship() {
+        return internship;
+    }
+
+    public void setInternship(Internship internship) {
+        this.internship = internship;
+    }
+
+    public Marks getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Marks marks) {
+        this.marks = marks;
+    }
+
+    public Skills getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Skills skills) {
+        this.skills = skills;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor tutor) {
+        this.tutor = tutor;
+    }
+
+    public Visit getVisit() {
+        return visit;
+    }
+
+    public void setVisit(Visit visit) {
+        this.visit = visit;
+    }
+}
