fe240625e2d05abacce08b5f10c78d8e1d1a0ec6
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-11-17 17:14:06 +0100

    add methods update in servlet

--- src/main/java/control/UpdateDetails.java
+++ src/main/java/control/UpdateDetails.java
@@ -1,6 +1,10 @@
 package control;
 
 
+import model.Student;
+import utils.database.InternshipDataServices;
+import utils.database.StudentDataServices;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +17,9 @@ public class UpdateDetails extends ServletModel{
     @Override
     public void init() {
         super.init();
+        InternshipDataServices internshipDataServices = new InternshipDataServices(dbUser, dbPwd, dbUrl);
+        StudentDataServices studentDataServices = new StudentDataServices(dbUser, dbPwd, dbUrl);
+        InternshipDataServices internshipDataServices = new InternshipDataServices(dbUser, dbPwd, dbUrl);
 
     }
 
@@ -20,23 +27,13 @@ public class UpdateDetails extends ServletModel{
         String detailsSubmitButton = request.getParameter("updateDetails");
         switch (detailsSubmitButton){
             case "company":
-                String companyName = request.getParameter("companyName");
-                String mds = request.getParameter("mds");
-                String begin =  request.getParameter("begin");
-                String end = request.getParameter("end");
-                String companyAddress = request.getParameter("companyAddress");
+                updateCompany(request);
                 break;
             case "student":
-                String studentId = request.getParameter("studentId");
-                String group = request.getParameter("group");
-                String firstName =  request.getParameter("firstName");
-                String lastName = request.getParameter("lastName");
-                String linkedin = request.getParameter("linkedin");
+                updateStudent(request);
                 break;
             case "internship":
-                String description = request.getParameter("description");
-                String tutorComments = request.getParameter("tutorComments");
-                String studentComments = request.getParameter("studentComments");
+                updateInternship(request);
                 break;
             case "keywords":
 //                TODO to be done
@@ -51,4 +48,38 @@ public class UpdateDetails extends ServletModel{
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
+
+
+    private void updateCompany(HttpServletRequest request){
+        String companyName = request.getParameter("companyName");
+        String mds = request.getParameter("mds");
+        String begin =  request.getParameter("begin");
+        String end = request.getParameter("end");
+        String companyAddress = request.getParameter("companyAddress");
+
+    }
+
+    private void updateStudent(HttpServletRequest request){
+        String studentId = request.getParameter("studentId");
+        String group = request.getParameter("group");
+        String firstName =  request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String linkedin = request.getParameter("linkedin");
+    }
+
+    private void updateInternship(HttpServletRequest request){
+        String description = request.getParameter("description");
+        String tutorComments = request.getParameter("tutorComments");
+        String studentComments = request.getParameter("studentComments");
+
+    }
+
+    private void updateSkills(HttpServletRequest request){
+
+    }
+
+    private void updateKeywords(HttpServletRequest request){
+
+    }
+
 }
