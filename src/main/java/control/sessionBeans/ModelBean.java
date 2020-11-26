3073781103eee3fe27810992c98423bc28da54bf
nerstak <roy.karst@gmail.com>
2020-11-26 13:06:52 +0100

    LastVal bug fixed

--- src/main/java/control/sessionBeans/ModelBean.java
+++ src/main/java/control/sessionBeans/ModelBean.java
@@ -2,7 +2,6 @@ package control.sessionBeans;
 
 import models.InterfaceEntity;
 
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.UUID;
@@ -11,12 +10,8 @@ public abstract class ModelBean<T extends InterfaceEntity> {
     @PersistenceContext
     protected EntityManager em;
 
-    public void save(T e) throws EntityExistsException {
-        if(!em.contains(e)) {
-            em.persist(e);
-        } else {
-            em.merge(e);
-        }
+    public void save(T e) {
+        em.merge(e);
     }
 
     public abstract T find(UUID id);
