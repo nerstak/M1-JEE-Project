ade791b3773ae3cc3cb0615d0409fc76fc942586
nerstak <roy.karst@gmail.com>
2020-11-27 12:50:25 +0100

    Small fixes

--- src/main/java/control/UpdateDetails.java
+++ src/main/java/control/UpdateDetails.java
@@ -30,14 +30,14 @@ public class UpdateDetails extends ServletModel{
     @EJB
     private CompanySessionBean companySB;
     
-    private final static String messageAttribute = "message";
-    private final static String internshipAttribute = "internshipId";
+    private static final String MESSAGE_ATTRIBUTE = "message";
+    private static final String INTERNSHIP_ATTRIBUTE = "internshipId";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Get the name of the button that call the servlet
         String detailsSubmitButton = request.getParameter("updateDetails");
-        String internshipId = request.getParameter(internshipAttribute);
+        String internshipId = request.getParameter(INTERNSHIP_ATTRIBUTE);
         InternshipEntity internshipEntity = internshipsSB.find(UUID.fromString(internshipId));
         boolean successRequest = false;
         switch (detailsSubmitButton){
@@ -80,20 +80,20 @@ public class UpdateDetails extends ServletModel{
         String companyId = request.getParameter("companyId");
         String companyAddress = request.getParameter("companyAddress");
             //Internship
-        String internshipId = request.getParameter(internshipAttribute);
+        String internshipId = request.getParameter(INTERNSHIP_ATTRIBUTE);
         String begin = request.getParameter("begin");
         String end = request.getParameter("end");
         String mds = request.getParameter("mds");
 
         //Check if all data are not empty
         if (ProcessString.areStringEmpty(companyId, internshipId, begin, end, mds, companyAddress, companyName)){
-            request.setAttribute(messageAttribute, ERR_EMPTY_FIELDS);
+            request.setAttribute(MESSAGE_ATTRIBUTE, ERR_EMPTY_FIELDS);
             return false;
         }
 
         //Check if all data are not empty
         if (ProcessString.isDateBefore(end, begin)){
-            request.setAttribute(messageAttribute, ERR_DATE_AFTER);
+            request.setAttribute(MESSAGE_ATTRIBUTE, ERR_DATE_AFTER);
             return false;
         }
 
@@ -132,7 +132,7 @@ public class UpdateDetails extends ServletModel{
 
         //Check if data are empty (expect linkedin url)
         if(ProcessString.areStringEmpty(studentId.toString(), firstName, lastName, email, group)){
-            request.setAttribute(messageAttribute, ERR_EMPTY_FIELDS);
+            request.setAttribute(MESSAGE_ATTRIBUTE, ERR_EMPTY_FIELDS);
             return false;
         }
         studentSB.save(student);
@@ -151,14 +151,14 @@ public class UpdateDetails extends ServletModel{
         String tutorComments = request.getParameter("tutorComments");
         String studentComments = request.getParameter("studentComments");
         String commentsId = request.getParameter("commentsId");
-        String internshipId = request.getParameter(internshipAttribute);
+        String internshipId = request.getParameter(INTERNSHIP_ATTRIBUTE);
         String titleId = request.getParameter("titleId");
         String title = request.getParameter("reportTitle");
 
 
         //Check if data(IDs) are empty
         if(ProcessString.areStringEmpty(titleId, commentsId, internshipId)){
-            request.setAttribute(messageAttribute, ERR_EMPTY_FIELDS);
+            request.setAttribute(MESSAGE_ATTRIBUTE, ERR_EMPTY_FIELDS);
             return false;
         }
 
@@ -185,7 +185,7 @@ public class UpdateDetails extends ServletModel{
 
         //Check if skill is empty
         if (skill.isEmpty()){
-            request.setAttribute(messageAttribute, ERR_EMPTY_FIELDS);
+            request.setAttribute(MESSAGE_ATTRIBUTE, ERR_EMPTY_FIELDS);
             return false;
         }
 
@@ -227,7 +227,7 @@ public class UpdateDetails extends ServletModel{
 
         //Check if skill is empty
         if (keyword.isEmpty()){
-            request.setAttribute(messageAttribute, ERR_EMPTY_FIELDS);
+            request.setAttribute(MESSAGE_ATTRIBUTE, ERR_EMPTY_FIELDS);
             return false;
         }
 
@@ -267,10 +267,10 @@ public class UpdateDetails extends ServletModel{
      */
     private void redirectToDetailsPage(HttpServletRequest request, HttpServletResponse response, InternshipEntity internshipEntity, boolean successRequest) {
         if (successRequest){
-            request.setAttribute(messageAttribute, SUCCESS_BD);
+            request.setAttribute(MESSAGE_ATTRIBUTE, SUCCESS_BD);
         }else{
-            if(request.getAttribute(messageAttribute) == null){
-                request.setAttribute(messageAttribute, ERR_FAILED_UPDATE_DB);
+            if(request.getAttribute(MESSAGE_ATTRIBUTE) == null){
+                request.setAttribute(MESSAGE_ATTRIBUTE, ERR_FAILED_UPDATE_DB);
             }
         }
 
