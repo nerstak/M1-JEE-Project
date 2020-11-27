7031c36a7cdc3dfe08b2422c8af7a712846c5461
Narice <narice@tutanota.com>
2020-11-27 15:55:31 +0100

    Added test for detail branch in Details.doPost

--- src/test/java/control/DetailsTest.java
+++ src/test/java/control/DetailsTest.java
@@ -18,6 +18,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import static utils.Constants.*;
 
@@ -58,7 +59,7 @@ public class DetailsTest {
     FinalReportSessionBean finalReportSB;
 
     @Mock
-    TutorEntity tutor;
+    InternshipEntity internshipEntity;
 
     @InjectMocks
     Details details;
@@ -72,5 +73,25 @@ public class DetailsTest {
 
         //Then
         then(response).should().sendRedirect(CONTROLLER_HOMEPAGE);
+    }
+
+    @Test
+    public void doPostDetailsTest() throws ServletException, IOException {
+        //Given
+        String internshipSubmit = "details";
+        String internshipId = "38400000-8cf0-11bd-b23e-10b96e4ef00d";
+        given(request.getParameter("internshipSubmit")).willReturn(internshipSubmit);
+        given(request.getParameter("internshipId")).willReturn(internshipId);
+        given(internshipsSB.find(UUID.fromString(internshipId))).willReturn(internshipEntity);
+        given(request.getRequestDispatcher(MISSION_PAGE)).willReturn(requestDispatcher);
+
+        //When
+        details.doPost(request, response);
+
+        //Then
+        then(request).should().setAttribute("internshipData", internshipEntity);
+        then(request).should().setAttribute("listOfSkills", skillsSB.getSkills());
+        then(request).should().setAttribute("listOfKeywords", keywordsSB.getKeywords());
+        then(requestDispatcher).should().forward(request, response);
     }
 }
